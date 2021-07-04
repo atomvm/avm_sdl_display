ec42e3c8f31d85e811c328317fafa4504196a106
Davide Bettio <davide.bettio@ispirata.com>
2021-07-04 13:56:42 +0200

    New painting implementation
    Add support for transparent background for text and images, out of screen items,
    and optimizations for hidden items, "one pass" update.
    This implementation line by line finds whether an item pixel is visible
    or not, avoiding useless drawings.

--- display.c
+++ display.c
@@ -15,6 +15,7 @@
 #define BPP 4
 #define DEPTH 32
 
+#define CHAR_WIDTH 8
 #include "font.c"
 
 // from AtomVM generic_unix_sys.h
@@ -51,6 +52,42 @@ struct KeyboardEvent
     bool key_down;
 };
 
+enum primitive
+{
+    Invalid = 0,
+    Image,
+    Rect,
+    Text
+};
+
+struct TextData
+{
+    Uint32 fgcolor;
+    const char *text;
+};
+
+struct ImageData
+{
+    const char *pix;
+};
+
+struct BaseDisplayItem
+{
+    enum primitive primitive;
+    int x;
+    int y;
+    int width;
+    int height;
+    uint32_t brcolor; /* bounding rect color */
+    union
+    {
+        struct ImageData image_data;
+        struct TextData text_data;
+    } data;
+};
+
+typedef struct BaseDisplayItem BaseDisplayItem;
+
 static term keyboard_pid;
 static struct timespec ts0;
 static int keyboard_event_fds[2];
@@ -62,71 +99,205 @@ static pthread_cond_t ready = PTHREAD_COND_INITIALIZER;
 static void consume_display_mailbox(Context *ctx);
 static void *display_loop();
 
-static void draw_image(SDL_Surface *screen, int x, int y, int width, int height, const char *data, Uint8 r, Uint8 g, Uint8 b)
+static inline Uint32 uint32_color_to_surface(SDL_Surface *screen, uint32_t color)
 {
-    Uint32 background_color = SDL_MapRGB(screen->format, r, g, b);
-    uint32_t *pixels = (uint32_t *) data;
+    return SDL_MapRGB(screen->format, (color >> 24) & 0xFF, (color >> 16) & 0xFF, (color >> 8) & 0xFF);
+}
 
-    for (int i = 0; i < height; i++) {
-        Uint32 *pixmem32 = (Uint32 *) (((uint8_t *) screen->pixels) + (screen->w * (y + i) + x) * BPP);
-        for (int j = 0; j < width; j++) {
-            Uint32 color;
-            if ((*pixels >> 24) & 0xFF) {
-                color = SDL_MapRGB(screen->format, (*pixels) & 0xFF, (*pixels >> 8) & 0xFF, (*pixels >> 16) & 0xFF);
-            } else {
-                color = background_color;
+static int draw_image_x(int xpos, int ypos, int max_line_len, BaseDisplayItem *item)
+{
+    int x = item->x;
+    int y = item->y;
+
+    Uint32 bgcolor;
+    bool visible_bg;
+    if (item->brcolor != 0) {
+        bgcolor = uint32_color_to_surface(screen, item->brcolor);
+        visible_bg = true;
+    } else {
+        visible_bg = false;
+    }
+
+    int width = item->width;
+    const char *data = item->data.image_data.pix;
+
+    int drawn_pixels = 0;
+
+    uint32_t *pixels = ((uint32_t *) data) + (ypos - y) * width + (xpos - x);
+    Uint32 *pixmem32 = (Uint32 *) (((uint8_t *) screen->pixels) + screen->w * ypos * BPP + xpos * BPP);
+
+    if (width > xpos - x + max_line_len) {
+        width = xpos - x + max_line_len;
+    }
+
+    for (int j = xpos - x; j < width; j++) {
+        uint32_t img_pixel = READ_32_UNALIGNED(pixels);
+        if ((*pixels >> 24) & 0xFF) {
+            Uint32 color = uint32_color_to_surface(screen, img_pixel);
+            pixmem32[drawn_pixels] = color;
+        } else if (visible_bg) {
+            pixmem32[drawn_pixels] = bgcolor;
+        } else {
+            return drawn_pixels;
+        }
+        drawn_pixels++;
+        pixels++;
+    }
+
+    return drawn_pixels;
+}
+
+static int draw_rect_x(int xpos, int ypos, int max_line_len, BaseDisplayItem *item)
+{
+    int x = item->x;
+    int width = item->width;
+    uint32_t color = uint32_color_to_surface(screen, item->brcolor);
+
+    int drawn_pixels = 0;
+
+    Uint32 *pixmem32 = (Uint32 *) (((uint8_t *) screen->pixels) + screen->w * ypos * BPP + xpos * BPP);
+
+    if (width > xpos - x + max_line_len) {
+        width = xpos - x + max_line_len;
+    }
+
+    for (int j = xpos - x; j < width; j++) {
+        pixmem32[drawn_pixels] = color;
+        drawn_pixels++;
+    }
+
+    return drawn_pixels;
+}
+
+static int draw_text_x(int xpos, int ypos, int max_line_len, BaseDisplayItem *item)
+{
+    int x = item->x;
+    int y = item->y;
+    Uint32 fgcolor = uint32_color_to_surface(screen, item->data.text_data.fgcolor);
+    Uint32 bgcolor;
+    bool visible_bg;
+    if (item->brcolor != 0) {
+        bgcolor = uint32_color_to_surface(screen, item->brcolor);
+        visible_bg = true;
+    } else {
+        visible_bg = false;
+    }
+
+    char *text = (char *) item->data.text_data.text;
+
+    int width = item->width;
+
+    int drawn_pixels = 0;
+
+    Uint32 *pixmem32 = (Uint32 *) (((uint8_t *) screen->pixels) + screen->w * ypos * BPP + xpos * BPP);
+
+    if (width > xpos - x + max_line_len) {
+        width = xpos - x + max_line_len;
+    }
+
+    for (int j = xpos - x; j < width; j++) {
+        int char_index = j / CHAR_WIDTH;
+        char c = text[char_index];
+        unsigned const char *glyph = fontdata + ((unsigned char) c) * 16;
+
+        unsigned char row = glyph[ypos - y];
+
+        bool opaque;
+        int k = j % CHAR_WIDTH;
+        if (row & (1 << (7 - k))) {
+            opaque = true;
+        } else {
+            opaque = false;
+        }
+
+        if (opaque) {
+            pixmem32[drawn_pixels] = fgcolor;
+        } else if (visible_bg) {
+            pixmem32[drawn_pixels] = bgcolor;
+        } else {
+            return drawn_pixels;
+        }
+        drawn_pixels++;
+    }
+
+    return drawn_pixels;
+}
+
+static int find_max_line_len(BaseDisplayItem *items, int count, int xpos, int ypos)
+{
+    int line_len = screen->w;
+
+    for (int i = 0; i < count; i++) {
+        BaseDisplayItem *item = &items[i];
+
+        if ((xpos < item->x) && (ypos >= item->y) && (ypos < item->y + item->height)) {
+            int len_to_item = item->x - xpos;
+            line_len = (line_len > len_to_item) ? len_to_item : line_len;
+        }
+    }
+
+    return line_len;
+}
+
+static int draw_x(int xpos, int ypos, BaseDisplayItem *items, int items_count)
+{
+    bool below = false;
+
+    for (int i = 0; i < items_count; i++) {
+        BaseDisplayItem *item = &items[i];
+        if ((xpos < item->x) || (xpos >= item->x + item->width) || (ypos < item->y) || (ypos >= item->y + item->height)) {
+            continue;
+        }
+
+        int max_line_len = below ? 1 : find_max_line_len(items, i, xpos, ypos);
+
+        int drawn_pixels = 0;
+        switch (items[i].primitive) {
+            case Image:
+                drawn_pixels = draw_image_x(xpos, ypos, max_line_len, item);
+                break;
+
+            case Rect:
+                drawn_pixels = draw_rect_x(xpos, ypos, max_line_len, item);
+                break;
+
+            case Text:
+                drawn_pixels = draw_text_x(xpos, ypos, max_line_len, item);
+                break;
+
+            default: {
+                fprintf(stderr, "unexpected display list command.\n");
             }
-            pixmem32[j] = color;
-            pixels++;
         }
+
+        if (drawn_pixels != 0) {
+            return drawn_pixels;
+        }
+
+        below = true;
     }
+
+    return 1;
 }
 
-static void draw_rect(SDL_Surface *screen, int x, int y, int width, int height, Uint8 r, Uint8 g, Uint8 b)
-{
-    Uint32 color = SDL_MapRGB(screen->format, r, g, b);
-
-    for (int i = 0; i < height; i++) {
-        Uint32 *pixmem32 = (Uint32 *) (((uint8_t *) screen->pixels) + (screen->w * (y + i) + x) * BPP);
-        for (int j = 0; j < width; j++) {
-            pixmem32[j] = color;
-        }
-    }
-}
-
-static void draw_text(SDL_Surface *screen, int x, int y, const char *text, Uint8 r, Uint8 g, Uint8 b, Uint8 bgr, Uint8 bgg, Uint8 bgb)
-{
-    int len = strlen(text);
-    Uint32 fgcolor = SDL_MapRGB(screen->format, r, g, b);
-    Uint32 bgcolor = SDL_MapRGB(screen->format, bgr, bgg, bgb);
-
-    for (int i = 0; i < len; i++) {
-        unsigned const char *glyph = fontdata + ((unsigned char) text[i]) * 16;
-
-        for (int j = 0; j < 16; j++) {
-            unsigned char row = glyph[j];
-
-            Uint32 *pixmem32 = (Uint32 *) (((uint8_t *) screen->pixels) + (screen->w * (y + j) + x + i * 8) * BPP);
-            for (int k = 0; k < 8; k++) {
-                if (row & (1 << (7 - k))) {
-                    pixmem32[k] = fgcolor;
-                } else {
-                    pixmem32[k] = bgcolor;
-                }
-            }
-        }
-    }
-}
-
-static void execute_command(Context *ctx, term req)
+static void init_item(BaseDisplayItem *item, term req, Context *ctx)
 {
     term cmd = term_get_tuple_element(req, 0);
 
     if (cmd == context_make_atom(ctx, "\x5"
                                       "image")) {
-        int x = term_to_int(term_get_tuple_element(req, 1));
-        int y = term_to_int(term_get_tuple_element(req, 2));
-        uint32_t bgcolor = term_to_int(term_get_tuple_element(req, 3));
+        item->primitive = Image;
+        item->x = term_to_int(term_get_tuple_element(req, 1));
+        item->y = term_to_int(term_get_tuple_element(req, 2));
+
+        term bgcolor = term_get_tuple_element(req, 3);
+        if (bgcolor == context_make_atom(ctx, "\xB"
+                                              "transparent")) {
+            item->brcolor = 0;
+        } else {
+            item->brcolor = ((uint32_t) term_to_int(bgcolor)) << 8 | 0xFF;
+        }
+
         term img = term_get_tuple_element(req, 4);
 
         term format = term_get_tuple_element(img, 0);
@@ -137,33 +308,35 @@ static void execute_command(Context *ctx, term req)
             fprintf(stderr, "\n");
             return;
         }
-        int width = term_to_int(term_get_tuple_element(img, 1));
-        int height = term_to_int(term_get_tuple_element(img, 2));
-        const char *data = term_binary_data(term_get_tuple_element(img, 3));
-
-        draw_image(screen, x, y, width, height, data, (bgcolor >> 16),
-            (bgcolor >> 8) & 0xFF, bgcolor & 0xFF);
+        item->width = term_to_int(term_get_tuple_element(img, 1));
+        item->height = term_to_int(term_get_tuple_element(img, 2));
+        item->data.image_data.pix = term_binary_data(term_get_tuple_element(img, 3));
 
     } else if (cmd == context_make_atom(ctx, "\x4"
                                              "rect")) {
-        int x = term_to_int(term_get_tuple_element(req, 1));
-        int y = term_to_int(term_get_tuple_element(req, 2));
-        int width = term_to_int(term_get_tuple_element(req, 3));
-        int height = term_to_int(term_get_tuple_element(req, 4));
-        int color = term_to_int(term_get_tuple_element(req, 5));
-
-        draw_rect(screen, x, y, width, height,
-            (color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF);
+        item->primitive = Rect;
+        item->x = term_to_int(term_get_tuple_element(req, 1));
+        item->y = term_to_int(term_get_tuple_element(req, 2));
+        item->width = term_to_int(term_get_tuple_element(req, 3));
+        item->height = term_to_int(term_get_tuple_element(req, 4));
+        item->brcolor = term_to_int(term_get_tuple_element(req, 5)) << 8 | 0xFF;
 
     } else if (cmd == context_make_atom(ctx, "\x4"
                                              "text")) {
-        int x = term_to_int(term_get_tuple_element(req, 1));
-        int y = term_to_int(term_get_tuple_element(req, 2));
-        term font = term_get_tuple_element(req, 3);
-        uint32_t fgcolor = term_to_int(term_get_tuple_element(req, 4));
-        uint32_t bgcolor = term_to_int(term_get_tuple_element(req, 5));
-        term text_term = term_get_tuple_element(req, 6);
+        item->primitive = Text;
+        item->x = term_to_int(term_get_tuple_element(req, 1));
+        item->y = term_to_int(term_get_tuple_element(req, 2));
 
+        item->data.text_data.fgcolor = term_to_int(term_get_tuple_element(req, 4));
+        term bgcolor = term_get_tuple_element(req, 5);
+        if (bgcolor == context_make_atom(ctx, "\xB"
+                                              "transparent")) {
+            item->brcolor = 0;
+        } else {
+            item->brcolor = ((uint32_t) term_to_int(bgcolor)) << 8 | 0xFF;
+        }
+
+        term font = term_get_tuple_element(req, 3);
         if (font != context_make_atom(ctx, "\xB"
                                            "default16px")) {
             fprintf(stderr, "unsupported font: ");
@@ -172,36 +345,80 @@ static void execute_command(Context *ctx, term req)
             return;
         }
 
+        term text_term = term_get_tuple_element(req, 6);
         int ok;
-        char *text = interop_term_to_string(text_term, &ok);
+        item->data.text_data.text = interop_term_to_string(text_term, &ok);
         if (!ok) {
             fprintf(stderr, "invalid text.\n");
             return;
         }
 
-        draw_text(screen, x, y, text, (fgcolor >> 16) & 0xFF, (fgcolor >> 8) & 0xFF, fgcolor & 0xFF,
-            (bgcolor >> 16) & 0xFF, (bgcolor >> 8) & 0xFF, bgcolor & 0xFF);
-
-        free(text);
+        item->height = 16;
+        item->width = strlen(item->data.text_data.text) * 8;
 
     } else {
         fprintf(stderr, "unexpected display list command: ");
         term_display(stderr, req, ctx);
         fprintf(stderr, "\n");
+
+        item->primitive = Invalid;
+        item->x = -1;
+        item->y = -1;
+        item->width = 1;
+        item->height = 1;
     }
 }
 
-static void execute_commands(Context *ctx, term display_list)
+static void destroy_items(BaseDisplayItem *items, int items_count)
 {
-    struct DisplayOpts *disp_opts = ctx->platform_data;
-    draw_rect(screen, 0, 0, disp_opts->width, disp_opts->height, 0xFF, 0xFF, 0xFF);
+    for (int i = 0; i < items_count; i++) {
+        BaseDisplayItem *item = &items[i];
+
+        switch (item->primitive) {
+            case Image:
+                break;
+
+            case Rect:
+                break;
+
+            case Text:
+                free((char *) item->data.text_data.text);
+                break;
+
+            default: {
+                break;
+            }
+        }
+    }
+
+    free(items);
+}
+
+static void do_update(Context *ctx, term display_list)
+{
+    int proper;
+    int len = term_list_length(display_list, &proper);
+
+    BaseDisplayItem *items = malloc(sizeof(BaseDisplayItem) * len);
 
     term t = display_list;
-
-    while (term_is_nonempty_list(t)) {
-        execute_command(ctx, term_get_list_head(t));
+    for (int i = len - 1; i >= 0; i--) {
+        init_item(&items[i], term_get_list_head(t), ctx);
         t = term_get_list_tail(t);
     }
+
+    int screen_width = screen->w;
+    int screen_height = screen->h;
+
+    for (int ypos = 0; ypos < screen_height; ypos++) {
+        int xpos = 0;
+        while (xpos < screen_width) {
+            int drawn_pixels = draw_x(xpos, ypos, items, len);
+            xpos += drawn_pixels;
+        }
+    }
+
+    destroy_items(items, len);
 }
 
 static void process_message(Context *ctx)
@@ -227,7 +444,7 @@ static void process_message(Context *ctx)
     if (cmd == context_make_atom(ctx, "\x6"
                                       "update")) {
         term display_list = term_get_tuple_element(req, 1);
-        execute_commands(ctx, display_list);
+        do_update(ctx, display_list);
 
     } else if (cmd == context_make_atom(ctx, "\xF"
                                              "subscribe_input")) {
@@ -418,6 +635,7 @@ void *display_loop(void *args)
                     event.key.keysym.unicode = '\n';
                 }
                 struct KeyboardEvent keyb_event;
+                memset(&keyb_event, 0, sizeof(struct KeyboardEvent));
                 keyb_event.key = event.key.keysym.unicode;
                 keyb_event.key_down = true;
                 write(keyboard_event_fds[1], &keyb_event, sizeof(keyb_event));
@@ -426,6 +644,7 @@ void *display_loop(void *args)
 
             case SDL_KEYUP: {
                 struct KeyboardEvent keyb_event;
+                memset(&keyb_event, 0, sizeof(struct KeyboardEvent));
                 keyb_event.key = event.key.keysym.sym;
                 keyb_event.key_down = false;
                 write(keyboard_event_fds[1], &keyb_event, sizeof(keyb_event));
